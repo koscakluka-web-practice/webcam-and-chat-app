93a591204030b273ab85df467a92896e12d12b01
Luka Košćak <koscak.luka@gmail.com>
2022-06-19 21:19:53 +0200

    Add Logging to the Server

--- backend/index.js
+++ backend/index.js
@@ -1,40 +1,43 @@
 import express from "express";
 import cors from "cors";
 
-import {createServer} from "http";
-import {Server} from "socket.io";
+import { createServer } from "http";
+import { Server } from "socket.io";
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 const server = createServer(app);
 const io = new Server(server, {
-    cors: {
-        origin: "*",
-        methods: ["GET", "POST"],
-    }
-})
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
 
 app.use(cors());
 
 app.get("/", (req, res) => {
-    res.send("Server running.");
+  res.send("Server running...");
 });
 
-io.on('connection', (socket) => {
-    socket.emit("me", socket.id);
+io.on("connection", (socket) => {
+  socket.emit("me", socket.id);
 
-    socket.on("disconnect", () => {
-        socket.broadcast.emit("call-ended");
-    });
+  socket.on("disconnect", () => {
+    console.log(`Client (#${socket.id}) Disconnected`);
+    socket.broadcast.emit("call-ended");
+  });
 
-    socket.on("call-user", ({calleeId, caller, signalData}) => {
-        io.to(calleeId).emit("call-user", {signal: signalData, caller});
-    });
+  socket.on("call-user", ({ calleeId, caller, signalData }) => {
+    console.log(`Calling user (#${calleeId} <= #${caller.id})`);
+    io.to(calleeId).emit("call-user", { signal: signalData, caller });
+  });
 
-    socket.on("answer-call", ({callerId, signal}) => {
-        io.to(callerId).emit("call-accepted", signal);
-    });
-})
+  socket.on("answer-call", ({ callerId, callee, signal }) => {
+    console.log(`Call accepted (#${callerId} => #${callee.id})`);
+    io.to(callerId).emit("call-accepted", { signal, callee });
+  });
+});
 
 server.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
