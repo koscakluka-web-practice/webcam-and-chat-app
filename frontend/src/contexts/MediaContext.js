a00b8e8e9187cce314f86d0ff2c45c21c08f3c34
Luka Košćak <koscak.luka@gmail.com>
2022-06-19 21:13:59 +0200

    Fix unknown MediaContext bug

--- frontend/src/contexts/MediaContext.js
+++ frontend/src/contexts/MediaContext.js
@@ -1,126 +1,149 @@
-import React, { createContext, useState, useRef, useEffect } from 'react';
-import {io} from "socket.io-client";
+import React, { createContext, useState, useRef, useEffect } from "react";
+import { io } from "socket.io-client";
 import Peer from "simple-peer";
 
 const MediaContext = createContext();
 
-const socket = io("http://localhost:5000");
+const socket = io("http://localhost:5000", { secure: true });
 
-const MediaProvider = ( {children} ) => {
-    const [me, setMe] = useState("");
-    const [name, setName] = useState("");
-    const [mediaStream, setMediaStream] = useState(null);
-    const [call, setCall] = useState(null);
-    const [callAccepted, setCallAccepted] = useState(false);
-    const [callEnded, setCallEnded] = useState(false);
+const MediaProvider = ({ children }) => {
+  const [me, setMe] = useState("");
+  const [name, setName] = useState("");
+  const [mediaStream, setMediaStream] = useState(null);
+  const [call, setCall] = useState(null);
+  const [callAccepted, setCallAccepted] = useState(false);
+  const [callEnded, setCallEnded] = useState(false);
 
+  const myMedia = useRef();
+  const receivingMedia = useRef();
 
-    const myMedia = useRef();
-    const receivingMedia = useRef();
+  const connectionRef = useRef();
 
-    const connectionRef = useRef();
+  useEffect(() => {
+    let retryMediaInterval = null;
 
-    useEffect(() => {
-        let retryMediaInterval = null;
+    navigator.mediaDevices
+      .getUserMedia({ video: true, audio: true })
+      .then((mediaStream) => {
+        setMediaStream(mediaStream);
 
-        navigator.mediaDevices.getUserMedia({ video: true, audio: true })
-            .then(mediaStream => {
-                setMediaStream(mediaStream);
-
-                if(myMedia.current) {
-                    myMedia.current.srcObject = mediaStream;
-                } else {
-                    retryMediaInterval = setInterval(() => {
-                        if (myMedia.current) {
-                            myMedia.current.srcObject = mediaStream;
-                            clearInterval(retryMediaInterval);
-                            retryMediaInterval = null;
-                        }
-                    }, 1000);
-                }
-            });
-
-        socket.on("me", id => {
-            setMe(id);
-        })
-
-        socket.on("call-user", ({signal, caller}) => {
-            setCall({isReceivingCall: true, caller, signal});
-        });
-
-        return () => {
-            if(retryMediaInterval) {
-                clearInterval(retryMediaInterval);
+        if (myMedia.current) {
+          myMedia.current.srcObject = mediaStream;
+        } else {
+          retryMediaInterval = setInterval(() => {
+            if (myMedia.current) {
+              myMedia.current.srcObject = mediaStream;
+              clearInterval(retryMediaInterval);
+              retryMediaInterval = null;
             }
+          }, 1000);
         }
-    }, []);
+      });
 
-    const answerCall = () => {
-        setCallAccepted(true);
+    socket.on("me", (id) => {
+      setMe(id);
+    });
 
-        const peer = new Peer({ initiator: false, trickle: false, mediaStream});
+    socket.on("call-user", ({ signal, caller }) => {
+      setCall({ isReceivingCall: true, caller, signal });
+    });
 
-        peer.on("signal", (data) => {
-            socket.emit("answer-call", {callerId: call.caller.id, signal: data})
-        });
+    return () => {
+      if (retryMediaInterval) {
+        clearInterval(retryMediaInterval);
+      }
+    };
+  }, []);
 
-        peer.on("stream", mediaStream => {
-            receivingMedia.current.srcObject = mediaStream;
-        });
+  const answerCall = () => {
+    setCallAccepted(true);
 
-        peer.signal(call.signal);
+    //console.log("Creating Peer");
+    const peer = new Peer({
+      initiator: false,
+      trickle: false,
+      stream: mediaStream,
+    });
 
-        connectionRef.current = peer;
-    }
+    peer.on("signal", (data) => {
+      //console.log("Answering call: ", data);
+      socket.emit("answer-call", {
+        callerId: call.caller.id,
+        callee: { id: me, name },
+        signal: data,
+      });
+    });
 
-    const callUser = (id) => {
+    peer.on("stream", (receivedStream) => {
+      //console.log("Received Stream (callee): ", receivedStream);
+      receivingMedia.current.srcObject = receivedStream;
+    });
 
-        const peer = new Peer({ initiator: true, trickle: false, mediaStream});
+    peer.signal(call.signal);
+    connectionRef.current = peer;
+  };
 
-        peer.on("signal", (data) => {
-            socket.emit("call-user", {caleeId: id, caller: {id: me, name}, signalData: data})
-        });
+  const callUser = (id) => {
+    //console.log("Creating Peer");
+    const peer = new Peer({
+      initiator: true,
+      trickle: false,
+      stream: mediaStream,
+    });
 
-        peer.on("stream", mediaStream => {
-            receivingMedia.current.srcObject = mediaStream;
-        });
+    peer.on("signal", (data) => {
+      //console.log("Calling user", data);
+      socket.emit("call-user", {
+        calleeId: id,
+        caller: { id: me, name },
+        signalData: data,
+      });
+    });
 
-        socket.on("call-accepted", signal => {
-            setCallAccepted(true);
+    peer.on("stream", (receivedStream) => {
+      //console.log("Received Stream (caller): ", receivedStream);
+      receivingMedia.current.srcObject = receivedStream;
+    });
 
-            peer.signal(signal);
-        })
+    socket.on("call-accepted", ({ signal, callee }) => {
+      //console.log("Call Accepted", signal, callee);
+      setCallAccepted(true);
+      setCall({ isReceivingCall: false, caller: callee, signal });
 
-        connectionRef.current = peer;
+      peer.signal(signal);
+    });
 
-    }
+    connectionRef.current = peer;
+  };
 
-    const leaveCall = () => {
-        setCallEnded(true);
+  const leaveCall = () => {
+    setCallEnded(true);
 
-        connectionRef.current.destory();
+    connectionRef.current.destroy();
 
-        window.location.reload();
-    }
+    window.location.reload();
+  };
 
-    return (
-        <MediaContext.Provider value={{
-            call,
-            callAccepted,
-            myMedia,
-            receivingMedia,
-            mediaStream,
-            callEnded,
-            me,
-            name,
-            setName,
-            callUser,
-            leaveCall,
-            answerCall
-        }}>
-            {children}
-        </MediaContext.Provider>
-    );
-}
+  return (
+    <MediaContext.Provider
+      value={{
+        call,
+        callAccepted,
+        myMedia,
+        receivingMedia,
+        mediaStream,
+        callEnded,
+        me,
+        name,
+        setName,
+        callUser,
+        leaveCall,
+        answerCall,
+      }}
+    >
+      {children}
+    </MediaContext.Provider>
+  );
+};
 
-export {MediaProvider, MediaContext};
+export { MediaProvider, MediaContext };
