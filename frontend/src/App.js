4aedd48575050b15daccdfcc2aa4ec09c7dd796e
Luka Košćak <koscak.luka@gmail.com>
2022-06-19 10:57:57 +0200

    Add MUI Theme Support

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,26 @@
-import {Typography, AppBar, createTheme, styled, Box} from "@mui/material"
 import { ThemeProvider } from "@emotion/react";
 
-const StyledAppBar = styled(AppBar)(({theme}) => ({
-  borderRadius: 15,
-  margin: '30px 100px',
-  display: 'flex',
-  flexDirection: 'row',
-  justifyContent: 'center',
-  alignItems: 'center',
-  width: '600px',
-  border: '2px solid black',
-}))
+import {Typography, createTheme, Box} from "@mui/material"
+import Header from "./components/Header";
 
-const Wrapper = styled(Box)(({theme}) => ({
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'center',
-  width: '100%',
-}))
-
-const theme = createTheme();
+const theme = createTheme({
+  palette: {
+    mode: 'dark'
+  }
+});
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
-      <Wrapper>
-        <StyledAppBar position="static" color="inherit">
+      <Box sx={{display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        width: '100%'
+      }}>
+        <Header position="static" color="inherit">
           <Typography variant="h2" align="center">Video Chat</Typography>
-        </StyledAppBar>
-      </Wrapper>
+        </Header>
+      </Box>
     </ThemeProvider>
   );
 }
